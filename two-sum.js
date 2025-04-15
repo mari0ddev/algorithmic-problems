68c093d4170f3bfa0672efbb2bd0f24723a32c60
mari0ddev <92933824+mari0ddev@users.noreply.github.com>
2025-04-15 12:43:47 +0300

    Update two-sum.js

--- two-sum.js
+++ two-sum.js
@@ -1,3 +1,15 @@
+//  Problem:
+// Given an array of integers (nums) and a target number,
+// return the indices of the two numbers that add up to the target.
+// You may assume that each input would have exactly one solution.
+
+// Example:
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
+
+
+
+
 function twoSum(nums, target) {
   const map = new Map();
 
